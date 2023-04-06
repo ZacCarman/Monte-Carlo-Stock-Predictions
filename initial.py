12b48e6863bd3a4077a5144d27e7614f9772d43e
AdrisAz <adris.azimi01@gmail.com>
2023-04-06 01:38:40 -0400

    Commented code

--- initial.py
+++ initial.py
@@ -1,18 +1,5 @@
-# import numpy as np
-# import pandas as pd
-# from pandas_datareader import data as wb
-# import matplotlib.pyplot as plt
-# import seaborn as sns
-# from scipy.stats import norm
 from datetime import datetime
 from datetime import date
-# ticker = 'GOOG'
-# data = pd.DataFrame()
-# startd = datetime(2019, 1, 1)
-# endd = date.today()
-# data[ticker] = wb.DataReader(ticker, data_source = 'yahoo', start = startd, end = endd )['Adj Close']
-# #Plot
-# data.plot(figsize=(15,6))
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as wb
@@ -24,6 +11,8 @@ import math
 from get_all_tickers import get_tickers as gt
 dict = ['GOOG', 'AAPL','MSFT','AMZN','NVDA']
 valid_Input = False
+
+# User ticker selection
 while not valid_Input:
     x = 0 
     val = -1
@@ -41,22 +30,29 @@ while not valid_Input:
         valid_Input = True
     else:
         print("Invalid input please try again \n\n")
+
+# Ticker data pull
 ticker =  dict[val]
 start = datetime(2019, 1, 1)
 end = date.today()
 data = yf.download(ticker, start , end)['Adj Close']
 data.plot(figsize=(15,6))
 plt.show()
+
+# Plot daily log returns
 log_returns = np.log(1 + data.pct_change())
-#Plot
 sns.distplot(log_returns.iloc[1:])
 plt.xlabel("Daily Return")
 plt.ylabel("Frequency")
 plt.show()
+
+# Probablitiy calculations
 u = log_returns.mean()
 var = log_returns.var()
 drift = u - (0.5*var)
 stdev = log_returns.std()
+
+# CAGR and initializations
 days = 50
 number_of_trials = 3000
 time_elapsed = (data.index[-1] - data.index[0]).days
@@ -65,6 +61,8 @@ number_of_years = time_elapsed / 365.0
 cagr = total_growth ** (1/number_of_years) - 1
 closing_prices = []
 number_of_trading_days = 252
+
+# Daily return percentages for the next 50 days and Monte Carlo Sim
 for i in range(number_of_trials):
     daily_return_percentages = np.random.normal(cagr/number_of_trading_days, stdev/math.sqrt(number_of_trading_days),number_of_trading_days)+1
     price_series = [data[-1]]
@@ -73,12 +71,17 @@ for i in range(number_of_trials):
         price_series.append(price_series[-1] * j)
     closing_prices.append(price_series[-1])
     plt.plot(price_series)
-
 plt.show()
+
+# Histogram of 50 day returns
 plt.hist(closing_prices,bins=40)
 plt.show()
+
+# Expected price
 mean_end_price = round(np.mean(closing_prices),2)
 print("Expected price: $", str(mean_end_price))
+
+# Modified histogram (Top & bottom 10 percentiles)
 top_ten = np.percentile(closing_prices,100-10)
 bottom_ten = np.percentile(closing_prices,10)
 plt.hist(closing_prices,bins=40)
